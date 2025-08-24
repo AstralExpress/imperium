8039ac95d262c67563340f2da43d239e46994584
SnowHawkeye <hadriensalem@gmail.com>
2025-08-24 15:39:03 +0200

    Update decorator

--- imperium/pathstrider.py
+++ imperium/pathstrider.py
@@ -1,9 +1,10 @@
 import pluggy
 
-hookspec = pluggy.HookspecMarker("orchestrator")
+hookspec = pluggy.HookspecMarker("pathstrider")
+PATHSTRIDER_MARKER = "pathstrider"
+
 
 class Pathstrider:
     @hookspec
     def hello(self, message: str):
         """Initialize plugin (prepare configs, volumes, etc.)."""
-
