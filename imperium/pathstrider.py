1ea2237ca05e9ccf459922c262647aa8a74e8b43
SnowHawkeye <hadriensalem@gmail.com>
2025-08-25 18:36:26 +0200

    Update hookspecs with basic lifecycle

--- imperium/pathstrider.py
+++ imperium/pathstrider.py
@@ -6,5 +6,13 @@ PATHSTRIDER_MARKER = "pathstrider"
 
 class Pathstrider:
     @hookspec
-    def hello(self, message: str):
+    def init_plugin(self, logger):
         """Initialize plugin (prepare configs, volumes, etc.)."""
+
+    @hookspec
+    def start_plugin(self):
+        """Start plugin containers, attach them to the given network."""
+
+    @hookspec
+    def stop_plugin(self):
+        """Stop plugin containers and clean up resources."""
